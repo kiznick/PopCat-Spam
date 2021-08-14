cc6fe2fb2ea4b720a0bebd4c79b0d87b2681133c
kiznick <22832362+kiznick@users.noreply.github.com>
2021-08-15 02:28:22 +0700

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+app = async () => {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--incognito',
+        ]
+    });
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36")
+    await page.goto('https://popcat.click/');
+    i = 0;
+    while(true) {
+        i++;
+        await page.click('[id="app"]');
+        console.log(`Click : ${i}`);
+        if (i % 800 === 0) {
+            console.log('Wait 5 Sec.')
+            await delay(5000);
+        }
+    }
+}
+app();
