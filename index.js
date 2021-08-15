ec7ef1be9d174438e1c468b4bb670e050c8c1b51
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2021-08-15 12:38:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app = async () => {
     i = 0;
     while(true) {
         i++;
-        await page.deleteCookie({"name": "ban"})
+        await page.deleteCookie({"name": "bot"})
         await page.click('[id="app"]');
         console.log(`Click : ${i}`);
         if (i % 799 === 0) {
